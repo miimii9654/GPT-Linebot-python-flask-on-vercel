2d45c922398d517009bdfd9fab7ec090942f063c
jacobL <ukljm.2005@gmail.com>
2023-10-06 16:39:45 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -13,6 +13,7 @@ working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true
 
 app = Flask(__name__)
 chatgpt = ChatGPT()
+CACHE = {} #付款用
 
 # domain root
 @app.route('/')
@@ -39,22 +40,7 @@ def handle_message(event):
     global working_status
     if event.message.type != "text":
         return
-    #-------------設定flex message----------------------------------
-    print(str(event))
-    print("--------------")
-    print(str(event.source))
-    tmp_obj = json.loads(str(event.source))
-    print("--------------")
-    #print(str(event.source.userId))
-    #print(event['source']['userId']))
-    print(str(tmp_obj))
-    print(str(tmp_obj['userId']))
-    print('--------------------')
     
-    line_id = str(tmp_obj['userId']) #event.source.userId
-    profile = line_bot_api.get_profile(line_id) # 取得line名稱
-    flex_content = get_flex_message_content(profile.display_name) # 設定flexmessage模板
-    #---------------------------------------------------------------
     if event.message.text == "說話":
         working_status = True
         line_bot_api.reply_message(
@@ -62,13 +48,31 @@ def handle_message(event):
             TextSendMessage(text="我可以說話囉，歡迎來跟我互動 ^_^ "))
         return
     if event.message.text == "pay":
+        product_name = 'AI敏捷專家Line諮詢(1小時)'
+        price = 99
+        order_id = str(uuid.uuid4())
+        amount = 1
+        currency = "TWD"
+        CACHE["order_id"] = order_id
+        CACHE["amount"] = amount
+        CACHE["currency"] = currency
+        #-------------設定flex message----------------------------------
+        print(str(event)) 
+        #tmp_obj = json.loads(str(event.source))
+        #line_id = str(tmp_obj['userId'])
+        line_id = json.loads(str(event.source))['userId']
+        profile = line_bot_api.get_profile(line_id) # 取得line名稱
+        
+        flex_content = get_flex_message_content(profile.display_name, order_id) # 設定flexmessage模板
+        #---------------------------------------------------------------
         line_bot_api.push_message(line_id, FlexSendMessage(
                             alt_text='hello',
                             contents=flex_content
                         ))
-        #line_bot_api.reply_message(
-        #    event.reply_token,
-        #    TextSendMessage(text="我可以說話囉，歡迎來跟我互動 ^_^ "))
+        
+        
+
+        
         return
         
     if event.message.text == "閉嘴":
