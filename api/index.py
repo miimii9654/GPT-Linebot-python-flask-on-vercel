4381673961f9e35c25962e8e15c57c6f3e395c03
jacobL <ukljm.2005@gmail.com>
2023-10-12 16:25:58 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -328,7 +328,7 @@ def check_useable(line_id):
     cur = conn.cursor()
     created_on = ''
     #cur.execute("select TO_CHAR(created_on, 'YYYY/MM/DD HH24:MI:SS') from aism_pay where rtnmsg='Succeeded' and line_id=%s order by created_on desc LIMIT 1",(line_id))
-    cur.execute("select TO_CHAR(created_on, 'YYYY/MM/DD HH24:MI:SS') from aism_pay where rtnmsg='Succeeded' and line_id='"+line_id+"' order by created_on desc LIMIT 1")
+    cur.execute("select TO_CHAR(created_on, 'YYYY/MM/DD HH24:MI:SS') from aism_pay where rtnmsg in ('Succeeded','paid') and line_id='"+line_id+"' order by created_on desc LIMIT 1")
     for r in cur :
         created_on=r[0]         
     current_time = datetime.now() + timedelta(hours=8)
